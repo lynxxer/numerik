15b4115572535716ebd43549ed2bfad1268ba299
Empyr <logonette123@gmail.com>
2020-07-07 18:46:32 +0200

    first changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,112 +1,165 @@
 import { Component, OnInit } from '@angular/core';
-import { LocationStrategy, PlatformLocation, Location } from '@angular/common';
-import { LegendItem, ChartType } from '../lbd/lbd-chart/lbd-chart.component';
-import * as Chartist from 'chartist';
+import random from 'highcharts-random'
+// Load Highcharts
+const Highcharts = require('highcharts');
+const random = require('highcharts-random')
 
+// Alternatively, this is how to load Highstock. Highmaps is similar.
+// var Highcharts = require('highcharts/highstock');
+ 
+// Load the exporting module, and initialize it.
+require('highcharts/modules/exporting')(Highcharts);
+//disabled for performance reasons for now:
+require('highcharts/modules/stock.src')(Highcharts);
+require('highcharts/modules/exporting.src')(Highcharts);
+require('highcharts/modules/offline-exporting.src')(Highcharts);
+require('highcharts/modules/export-data.src')(Highcharts);
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-    public emailChartType: ChartType;
-    public emailChartData: any;
-    public emailChartLegendItems: LegendItem[];
+  public count = 0 ;
 
-    public hoursChartType: ChartType;
-    public hoursChartData: any;
-    public hoursChartOptions: any;
-    public hoursChartResponsive: any[];
-    public hoursChartLegendItems: LegendItem[];
-
-    public activityChartType: ChartType;
-    public activityChartData: any;
-    public activityChartOptions: any;
-    public activityChartResponsive: any[];
-    public activityChartLegendItems: LegendItem[];
   constructor() { }
 
+  incNumber() {
+    setInterval(() => {
+      this.count++;
+    }, 2000)
+
+  }
+
   ngOnInit() {
-      this.emailChartType = ChartType.Pie;
-      this.emailChartData = {
-        labels: ['62%', '32%', '6%'],
-        series: [62, 32, 6]
-      };
-      this.emailChartLegendItems = [
-        { title: 'Open', imageClass: 'fa fa-circle text-info' },
-        { title: 'Bounce', imageClass: 'fa fa-circle text-danger' },
-        { title: 'Unsubscribe', imageClass: 'fa fa-circle text-warning' }
-      ];
 
-      this.hoursChartType = ChartType.Line;
-      this.hoursChartData = {
-        labels: ['9:00AM', '12:00AM', '3:00PM', '6:00PM', '9:00PM', '12:00PM', '3:00AM', '6:00AM'],
-        series: [
-          [287, 385, 490, 492, 554, 586, 698, 695, 752, 788, 846, 944],
-          [67, 152, 143, 240, 287, 335, 435, 437, 539, 542, 544, 647],
-          [23, 113, 67, 108, 190, 239, 307, 308, 439, 410, 410, 509]
-        ]
-      };
-      this.hoursChartOptions = {
-        low: 0,
-        high: 800,
-        showArea: true,
-        height: '245px',
-        axisX: {
-          showGrid: false,
-        },
-        lineSmooth: Chartist.Interpolation.simple({
-          divisor: 3
-        }),
-        showLine: false,
-        showPoint: false,
-      };
-      this.hoursChartResponsive = [
-        ['screen and (max-width: 640px)', {
-          axisX: {
-            labelInterpolationFnc: function (value) {
-              return value[0];
-            }
+ // Create the chart
+ Highcharts.chart('container', {
+  chart: {
+      type: 'column',
+      renderTo: 'container',
+      backgroundColor: 'rgba(0,0,0,0)'
+  },
+  title: {
+      text: 'Banesat e shitura gjat vitit te kaluar'.toUpperCase()
+  },
+  xAxis: {
+      categories: [
+          'Jan',
+          'Feb',
+          'Mar',
+          'Apr',
+          'May',
+          'Jun',
+          'Jul',
+          'Aug',
+          'Sep',
+          'Oct',
+          'Nov',
+          'Dec'
+      ],
+      crosshair: true
+  },
+  yAxis: {
+      min: 0,
+      allowDecimals: false,
+      title: {
+          text: 'Numri i Banesave'
+      }
+  },
+  tooltip: {
+      headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+      pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' + 
+          '<td style="padding:0"><b>{point.y:.0f} banesa</b></td></tr>',
+      footerFormat: '</table>',
+      useHTML: true
+  },
+  plotOptions: {
+      column: {
+          pointPadding: 0.2,
+          borderWidth: 0
+      }
+  },
+  series: [{
+      name: 'Dardani',
+      data: random.data()
+
+  }, {
+      name: 'Pejton',
+      data: random.data()
+
+  }, {
+      name: 'Bregu Diellit',
+      data: random.data()
+
+  }, {
+      name: 'Emshir',
+      data: random.data()
+
+  }]
+});
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//SECOND CHART.!
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Highcharts.chart('secondContainer', {
+  chart: {
+      type: 'bar',
+      renderTo: 'secondContainer',
+      backgroundColor: 'rgba(0,0,0,0)'
+  },
+  title: {
+      text: 'Historic World Population by Region'
+  },
+  subtitle: {
+      text: null
+  },
+  xAxis: {
+      categories: ['Dardani', 'Pejton', 'Bregu i Diellit', 'Emshir', 'Other'],
+      title: {
+          text: null
+      }
+  },
+  yAxis: {
+      min: 0,
+      title: {
+          text: 'Numri i Banesave',
+      },
+      labels: {
+          overflow: 'justify'
+      }
+  },
+  tooltip: {
+      valueSuffix: ' millions'
+  },
+  plotOptions: {
+      bar: {
+          dataLabels: {
+              enabled: true
           }
-        }]
-      ];
-      this.hoursChartLegendItems = [
-        { title: 'Open', imageClass: 'fa fa-circle text-info' },
-        { title: 'Click', imageClass: 'fa fa-circle text-danger' },
-        { title: 'Click Second Time', imageClass: 'fa fa-circle text-warning' }
-      ];
-
-      this.activityChartType = ChartType.Bar;
-      this.activityChartData = {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        series: [
-          [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
-          [412, 243, 280, 580, 453, 353, 300, 364, 368, 410, 636, 695]
-        ]
-      };
-      this.activityChartOptions = {
-        seriesBarDistance: 10,
-        axisX: {
-          showGrid: false
-        },
-        height: '245px'
-      };
-      this.activityChartResponsive = [
-        ['screen and (max-width: 640px)', {
-          seriesBarDistance: 5,
-          axisX: {
-            labelInterpolationFnc: function (value) {
-              return value[0];
-            }
-          }
-        }]
-      ];
-      this.activityChartLegendItems = [
-        { title: 'Tesla Model S', imageClass: 'fa fa-circle text-info' },
-        { title: 'BMW 5 Series', imageClass: 'fa fa-circle text-danger' }
-      ];
-
-
-    }
-
+      }
+  },
+  legend: {
+    showLegend: true,
+  },
+  credits: {
+      enabled: false
+  },
+  series: [{
+      name: 'Year 2017',
+      data: random.data(5)
+  }, {
+      name: 'Year 2018',
+      data: random.data(5)
+  }, {
+      name: 'Year 2019',
+      data: random.data(5)
+  }, {
+      name: 'Year 2020',
+      data: random.data(5)
+      }]
+    });
+}    
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//INCREMENT NUMBER TEST !
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
